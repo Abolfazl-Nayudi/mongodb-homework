da96498093b5130569590a7176912cfe18735a27
Abolfazl Nayudi <abolfazl.nayudi@gmail.com>
2024-04-17 22:59:32 +0430

    push client code

---
+++ scripts/header.js
@@ -0,0 +1,24 @@
+const header = document.querySelector("header");
+
+header.innerHTML = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+<a class="navbar-brand" href="/">Homework</a>
+<button
+  class="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNavAltMarkup"
+  aria-controls="navbarNavAltMarkup"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+    <a class="nav-link active" aria-current="page" href="/">Home</a>
+    <a class="nav-link" href="./pages/addPerson.html">add person</a>
+  </div>
+</div>
+</div>
+</nav>`;
