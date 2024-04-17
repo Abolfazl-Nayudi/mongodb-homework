71c58f7994684cb289bf2b57db4928dddc153f52
Abolfazl Nayudi <abolfazl.nayudi@gmail.com>
2024-04-17 21:50:54 +0430

    add cors library

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { router } = require('./routes/faculty.route');
 const { db } = require('./db/mongo.config');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
